3128f552f572d86de50af116c773c43b3a650a90
lroca7 <lizethcabrales@gmail.com>
2020-07-18 16:23:25 -0500

    menu mobile

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,14 @@
 <template>
     <v-app id="inspire" class="jajajaja">
-        <!-- <v-navigation-drawer v-model="drawer" temporary="temporary" app>
-            <v-list dense>
-                <v-list-item link>
-                    <v-list-item-action>
-                        <v-icon>mdi-home</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Home</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item link>
-                    <v-list-item-action>
-                        <v-icon>mdi-email</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Contact</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer> -->
-
-        <!-- <v-app-bar app class="wine-ligth">
-            <v-app-bar-nav-icon
-                class="menu-icon"
-                @click.stop="drawer = !drawer"
-            ></v-app-bar-nav-icon>
-            <div class="toolbar">
-                <v-toolbar-title>ATIX</v-toolbar-title> -->
-        <px-header :links="links"></px-header>
-        <!-- </div>
-        </v-app-bar> -->
-
+        <px-header :links="links" class="hidden-sm-and-down"></px-header>
+        <PxHeaderMobile
+            :links="links"
+            class="hidden-lg-and-up"
+        ></PxHeaderMobile>
         <v-main>
             <v-container class="main__container pa-8" fluid>
                 <router-view></router-view>
             </v-container>
-            <!-- <v-container class="fill-height" fluid>
-
-                <v-row align="center" justify="center">
-                    <v-col class="text-center">
-                        
-                         <v-tooltip left>
-                            <template v-slot:activator="{ on }">
-                                <v-btn
-                                    :href="source"
-                                    icon
-                                    large
-                                    target="_blank"
-                                    v-on="on"
-                                >
-                                    <v-icon large>mdi-code-tags</v-icon>
-                                </v-btn>
-                            </template>
-                            <span>Source</span>
-                        </v-tooltip> 
-                    </v-col>
-                </v-row> 
-            </v-container> -->
         </v-main>
 
         <v-footer app class="wine-dark">
@@ -68,10 +19,12 @@
 
 <script>
 import PxHeader from '@/components/PxHeader'
+import PxHeaderMobile from '@/components/PxHeaderMobile'
 
 export default {
     components: {
-        PxHeader
+        PxHeader,
+        PxHeaderMobile
     },
     props: {
         source: String
---
+++ src/components/PxHeaderMobile.vue
@@ -0,0 +1,115 @@
+<template>
+    <div>
+        <v-navigation-drawer v-model="drawer" temporary="temporary" app>
+            <v-list dense class="submenu">
+                <v-list-item link text v-for="s in menus" :key="`${s.id}`">
+                    <!-- <v-list-item-action>
+                        <v-icon>{{ s.icon }}</v-icon>
+                    </v-list-item-action> -->
+                    <v-list-item-content>
+                        <v-list-item-title>
+                            <router-link :to="s.slug">
+                                <span>
+                                    {{ s.name }}
+                                </span>
+                            </router-link>
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar app class="wine-ligth">
+            <v-app-bar-nav-icon
+                class="menu-icon"
+                @click.stop="drawer = !drawer"
+            ></v-app-bar-nav-icon>
+            <div class="toolbar">
+                <v-toolbar-title class="company_title">
+                    <router-link to="/">
+                        <b>
+                            ATIX
+                        </b>
+                    </router-link>
+                </v-toolbar-title>
+                <v-toolbar-items class="hidden-sm-and-down">
+                    <v-btn
+                        text
+                        v-for="m in menus"
+                        :key="`${m.id}`"
+                        v-on:click="openSubMenu(m.id)"
+                    >
+                        <router-link :to="m.slug">
+                            <span>
+                                {{ m.name }}
+                            </span>
+                        </router-link>
+                    </v-btn>
+                </v-toolbar-items>
+
+            </div>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PxHeaderMobile',
+    components: {},
+
+    data() {
+        return {
+            drawer: null,
+            menus: [],
+            submenus: []
+        }
+    },
+
+    props: {
+        links: {
+            type: Array,
+            default: () => {}
+        }
+    },
+
+    created() {
+        let me = this
+
+        fetch(`${this.$apiUrl}menu/list`)
+            .then(response => response.json())
+            .then(dataItems => {
+                me.menus = dataItems.data
+            })
+    },
+
+    methods: {
+        openSubMenu(menuId) {
+            let me = this
+
+            fetch(`${this.$apiUrl}menu/submenus/${menuId}`)
+                .then(response => response.json())
+                .then(dataItems => {
+                    if (dataItems.length > 0) {
+                        me.submenus = dataItems
+                        me.drawer = true
+                    }
+                })
+        }
+    }
+}
+</script>
+
+<style>
+.submenu span {
+    color: #c10000;
+    font-size: 0.9rem;
+}
+.toolbar a {
+    color: #ffffff !important;
+    /* text-decoration: none; */
+}
+.company_title a {
+    font-size: 2rem;
+    font-family: 'Anton', Courier, monospace;
+}
+</style>
